f0eaf969fd2c8c3bdd486ec62902bbd66320870e
Santosh Kumar <santoshkumarg227@gmail.com>
2025-07-30 16:19:14 +0530

    implemented fetch single flight

--- flight-management-service/src/controllers/Flight-controller.js
+++ flight-management-service/src/controllers/Flight-controller.js
@@ -44,6 +44,20 @@ const getAllFlights = async (req, res) => {
   }
 };
 
+const getFlight = async (req,res)=>{
+  try {
+    const { id } = req.params; // Assuming route is /api/flights/:id
+    const flight = await Flight.findById(id);
+
+    if (!flight) {
+      return res.status(404).json({ message: 'Flight not found.' });
+    }
+    res.status(200).json(flight);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err.message });
+  }
+}
+
 const getAllFlightsByFilter = async (req, res) => {
   try {
     const filter = {};
@@ -108,4 +122,4 @@ const getAllFlightsByFilter = async (req, res) => {
   }
 };
 
-module.exports = { createFlight, getAllFlights, getAllFlightsByFilter };
+module.exports = { createFlight, getAllFlights, getFlight,getAllFlightsByFilter };
--- flight-management-service/src/routes/v1/FlightRoute.js
+++ flight-management-service/src/routes/v1/FlightRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const {createFlight,getAllFlightsByFilter} = require('../../controllers/Flight-controller')// Destructure to get the function
+const {createFlight,getAllFlightsByFilter,getFlight} = require('../../controllers/Flight-controller')// Destructure to get the function
 const flightMiddleware = require('../../middlewares/flightMiddleware')   // fix the case if needed
 
 router.post('/',flightMiddleware, createFlight);
 router.get('/', getAllFlightsByFilter);
+router.get('/:id', getFlight);
 router.get('/trips/:departureAirportId-:arrivalAirportId', getAllFlightsByFilter);
 
 
